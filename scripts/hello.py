758382895ba1d455604cea0b8328b153a94f1d45
andaritri <95196590+andaritri@users.noreply.github.com>
2021-12-15 22:50:35 -0500

    Show python version

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import sys
 
-def print_message():
+
+def print_message(message="Hello"):
     """print message"""
-    for _ in range(10):
-        print("from caller workflow!")
+    for _ in range(5):
+        print("This was the message passed as argument: {}".format(message))
 
         
 if __name__ == "__main__":
     print_message()
+    print("This script was executed using Python {}".format(sys.version))
