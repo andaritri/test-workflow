bc8c805b24f5b66c25d4ab6d123772f0afb657ec
andaritri <95196590+andaritri@users.noreply.github.com>
2021-12-15 21:38:18 -0500

    Add function

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+def print_message():
+    """print message"""
+    for _ in range(10):
+        print("from caller workflow!")
 
-print("from caller workflow!")
+        
+if __name__ == "__main__":
+    print_message()
